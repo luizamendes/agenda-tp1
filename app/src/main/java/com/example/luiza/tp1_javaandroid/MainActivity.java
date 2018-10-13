0ac4a429ad45d612b38363fd4fca9696a7c64ae5
Luiza <luizaleitemendes@gmail.com>
2018-10-13 19:15:23 -0300

    First Commit

---
+++ app/src/main/java/com/example/luiza/tp1_javaandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.luiza.tp1_javaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
